b45a336ff18fa450e26e3a5cf99a769af9dc892b
Thomas <71355143+thomas694@users.noreply.github.com>
2021-01-04 22:53:33 +0100

    updated copyright year

--- src/TumblThree/SharedAssemblyInfo.cs
+++ src/TumblThree/SharedAssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TumblThree")]
-[assembly: AssemblyCopyright("Copyright (C) 2016-2020 Johannes Meyer zum Alten Borgloh")]
+[assembly: AssemblyCopyright("Copyright (C) 2016-2021 Johannes Meyer zum Alten Borgloh and others")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)] // The WinRT API is not CLS-compliant (e.g. usage of uint)
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
-[assembly: AssemblyVersion("1.0.11.8")]
-[assembly: AssemblyFileVersion("1.0.11.8")]
+[assembly: AssemblyVersion("1.0.12.1")]
+[assembly: AssemblyFileVersion("1.0.12.1")]
