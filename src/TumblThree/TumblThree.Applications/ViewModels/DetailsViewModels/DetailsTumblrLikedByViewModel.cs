4ec4372af427dbb3f879698fe338bdfd09ed66f4
Thomas <71355143+thomas694@users.noreply.github.com>
2021-04-26 22:52:09 +0200

    Refactor Global exception handling
    - The just implemented global exception handling didn't always work. Besides the dialog doesn't look very good.
    - A new error window for showing global exceptions has been introduced. The exception information can be copied.

--- src/TumblThree/TumblThree.Applications/ViewModels/DetailsViewModels/DetailsTumblrLikedByViewModel.cs
+++ src/TumblThree/TumblThree.Applications/ViewModels/DetailsViewModels/DetailsTumblrLikedByViewModel.cs
@@ -37,7 +37,7 @@ namespace TumblThree.Applications.ViewModels.DetailsViewModels
 
         public void ViewLostFocus()
         {
-            if (Count == 1) BlogFile.Save();
+            if (Count == 1 && BlogFile != null) BlogFile.Save();
         }
 
         public bool FilenameTemplateValidate(string enteredFilenameTemplate)
---
+++ src/TumblThree/TumblThree.Presentation/Controls/SelectableTextBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TumblThree.Presentation.Extensions;
+
+namespace TumblThree.Presentation.Controls
+{
+    public class SelectableTextBlock : TextBlock
+    {
+        static SelectableTextBlock()
+        {
+            FocusableProperty.OverrideMetadata(typeof(SelectableTextBlock), new FrameworkPropertyMetadata(true));
+            TextEditorWrapper.RegisterCommandHandlers(typeof(SelectableTextBlock), true, true, true);
+
+            // remove the focus rectangle around the control
+            FocusVisualStyleProperty.OverrideMetadata(typeof(SelectableTextBlock), new FrameworkPropertyMetadata((object)null));
+        }
+
+        private readonly TextEditorWrapper _editor;
+
+        public SelectableTextBlock()
+        {
+            _editor = TextEditorWrapper.CreateFor(this);
+        }
+    }
+}
