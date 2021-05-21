9301aa813f2219ebedf21974ea6fa4a2c915c2b9
Thomas <71355143+thomas694@users.noreply.github.com>
2021-05-21 22:57:59 +0200

    Refactor BrushResourceConverter from Applications to Presentation

--- src/TumblThree/TumblThree.Presentation/Converters/BrushResourceConverter.cs
+++ src/TumblThree/TumblThree.Presentation/Converters/BrushResourceConverter.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
-namespace TumblThree.Applications.Converter
+namespace TumblThree.Presentation.Converters
 {
     public class BrushResourceConverter : IValueConverter
     {
