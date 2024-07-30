20823635ef3806ed859589509c76ac3e1410b49d
Thomas <71355143+thomas694@users.noreply.github.com>
2024-07-30 22:44:55 +0200

    Add Display a marker in the queue view indicating a rescan
    - Either using the 'Force rescan' blog option or as long as the LastId of the blog is 0, a full rescan of the blog will be performed.
    - Since this can take a long time depending on the crawler and blog size, a marker ("[F]") is displayed after the blog name in the queue.

---
+++ src/TumblThree/TumblThree.Presentation/Converters/BlogQueueIndicatorConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TumblThree.Presentation.Converters
+{
+    public class BlogQueueIndicatorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values is null || values.Length < 2 ? "" : $"{((bool)values[0] || (ulong)values[1] == 0 ? "[F]" : "")}";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) =>
+            throw new NotSupportedException();
+    }
+}
